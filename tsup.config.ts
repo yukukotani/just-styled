6dc476dfc2541090b1677a008a3087f9510d4552
Yuku Kotani <yukukotani@gmail.com>
2024-10-12 13:54:14 +0900

    fix build

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
+	entry: ["./src", "!src/**/*.test.ts", "!src/__snapshots__/**/*"],
 	format: ["cjs", "esm"],
 	dts: true,
 	clean: true,
