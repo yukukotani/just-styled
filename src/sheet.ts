a21865b4a10dfcf04c532dba740b56cb4cfbfd0e
Yuku Kotani <yukukotani@gmail.com>
2024-10-12 13:23:07 +0900

    refactor

---
+++ src/sheet.ts
@@ -0,0 +1,34 @@
+import fnv1a from "@sindresorhus/fnv1a";
+import type { CSSProperties } from "react";
+
+type Sheet = {
+	className: string;
+	css: string;
+};
+
+export function generateSheets(props: CSSProperties) {
+	const sheets: Sheet[] = [];
+
+	for (const [key, value] of Object.entries(props)) {
+		if (value) {
+			const kebab = toKebabCase(key);
+
+			const hash = fnv1a(`${key}${value}`, { size: 32 })
+				.toString(36)
+				.slice(0, 4);
+			// appends prefix to avoid a leading number which is invalid as a className
+			const className = `j${hash}`;
+
+			sheets.push({
+				className: className,
+				css: `.${className} { ${kebab}: ${value}; }`,
+			});
+		}
+	}
+
+	return sheets;
+}
+
+function toKebabCase(str: string) {
+	return str.replace(/([a-z])([A-Z])/g, "$1-$2").toLowerCase();
+}
--- src/styled.tsx
+++ src/styled.tsx
@@ -1,6 +1,6 @@
-import fnv1a from "@sindresorhus/fnv1a";
 import type { CSSProperties, ComponentProps, ElementType } from "react";
 import React from "react";
+import { generateSheets } from "./sheet";
 
 export function styled<C extends ElementType<{ className?: string }>>(
 	Component: C,
@@ -13,7 +13,7 @@ export function styled<C extends ElementType<{ className?: string }>>(
 			<>
 				{sheets.map((s) => (
 					<style key={s.className} href={s.className} precedence="medium">
-						{s.sheet}
+						{s.css}
 					</style>
 				))}
 				{/* @ts-expect-error */}
@@ -24,27 +24,4 @@ export function styled<C extends ElementType<{ className?: string }>>(
 			</>
 		);
 	};
-}
-
-function generateSheets(props: CSSProperties) {
-	const sheets: { className: string; sheet: string }[] = [];
-
-	for (const [key, value] of Object.entries(props)) {
-		if (value) {
-			const kebab = toKebabCase(key);
-			const hash = fnv1a(`${key}${value}`, { size: 32 })
-				.toString(36)
-				.slice(0, 4);
-			const className = `x${hash}`;
-			sheets.push({
-				className: className,
-				sheet: `.${className} { ${kebab}: ${value}; }`,
-			});
-		}
-	}
-
-	return sheets;
-}
-function toKebabCase(str: string) {
-	return str.replace(/([a-z])([A-Z])/g, "$1-$2").toLowerCase();
 }
