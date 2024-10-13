08d77b8ac0eb85630a29ca1cea679bd26554be14
Yuku Kotani <yukukotani@gmail.com>
2024-10-13 16:29:07 +0900

    ignore tests on build

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-	entry: ["./src", "!src/**/*.test.ts", "!src/__snapshots__/**/*"],
+	entry: ["./src", "!src/**/*.test.*", "!src/__snapshots__/**/*"],
 	format: ["cjs", "esm"],
 	dts: true,
 	clean: true,
