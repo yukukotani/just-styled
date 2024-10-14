b59c216751e7df6fa8c919288f28ac473ae34729
Yuku Kotani <yukukotani@gmail.com>
2024-10-14 15:13:41 +0900

    update test

--- src/styled.test.tsx
+++ src/styled.test.tsx
@@ -7,32 +7,10 @@ describe("styled", async () => {
 	test("renders style tag in head", async () => {
 		const RedDiv = styled("div", {
 			color: "red",
+			gap: "$spaces.md",
 		});
 
 		render(<RedDiv data-testid="red-div">RedDiv</RedDiv>);
-
-		await waitFor(() => {
-			const styleTag = Array.from(document.getElementsByTagName("style")).find(
-				(e) => e.dataset.href != null,
-			);
-			expect(styleTag).toBeDefined();
-			// biome-ignore lint/style/noNonNullAssertion: <explanation>
-			expect(document.head).toContainElement(styleTag!);
-			expect(styleTag).toHaveAttribute("data-precedence", "medium");
-			expect(styleTag).not.toBeEmptyDOMElement();
-		});
-	});
-
-	test("renders dynamic style", async () => {
-		const RedDiv = styled("div", {
-			color: "red",
-		});
-
-		render(
-			<RedDiv data-testid="red-div" style={{ backgroundColor: "gray" }}>
-				RedDiv
-			</RedDiv>,
-		);
 
 		await waitFor(() => {
 			const styleTags = Array.from(
@@ -42,7 +20,34 @@ describe("styled", async () => {
 			for (const styleTag of styleTags) {
 				expect(document.head).toContainElement(styleTag);
 				expect(styleTag).toHaveAttribute("data-precedence", "medium");
-				expect(styleTag).not.toBeEmptyDOMElement();
+				expect(styleTag.textContent).toMatchSnapshot();
+			}
+		});
+	});
+
+	test("renders dynamic style", async () => {
+		const RedDiv = styled("div", {
+			color: "red",
+		});
+
+		render(
+			<RedDiv
+				data-testid="red-div"
+				style={{ backgroundColor: "gray", gap: "$spaces.md" }}
+			>
+				RedDiv
+			</RedDiv>,
+		);
+
+		await waitFor(() => {
+			const styleTags = Array.from(
+				document.getElementsByTagName("style"),
+			).filter((e) => e.dataset.href != null);
+			expect(styleTags).toHaveLength(3);
+			for (const styleTag of styleTags) {
+				expect(document.head).toContainElement(styleTag);
+				expect(styleTag).toHaveAttribute("data-precedence", "medium");
+				expect(styleTag.textContent).toMatchSnapshot();
 			}
 		});
 	});
